2c4758c10170957db9b407162ed52003ffecd8b0
ducdang93 <duc.dang@datagap.io>
2024-02-26 23:02:39 +0700

    first commit

---
+++ DesignPatterns/Program.cs
@@ -0,0 +1,51 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace DesignPatterns
+{
+    public class Journal{
+        private readonly List<string> entries = new List<string>();
+        private static int count = 0;
+
+        public int AddEntry(string text){
+            entries.Add($"{++count}: {text}");
+            return count;
+        }
+        public void RemoveEntry(int index){
+            entries.RemoveAt(index);
+        }
+        public override string ToString(){
+            return string.Join(Environment.NewLine, entries);
+        }
+        
+    }
+
+    public class Persistence{
+        public void SaveToFile(Journal j, string filename, bool overwrite = false){
+            if(overwrite || !File.Exists(filename))
+                File.WriteAllText(filename, j.ToString());
+
+        }
+    }
+
+    public class Demo{
+        static void Main(string[] args){
+            var j = new Journal();
+            j.AddEntry("I cried today.");
+            j.AddEntry("I ate a bug.");
+            WriteLine(j);
+
+            var p = new Persistence();
+            var filename = @"D:\learn\DesignPatterns\DesignPatterns\journal.txt";
+            p.SaveToFile(j, filename, true);
+            Process.Start(filename);
+        }
+    }
+}
+
